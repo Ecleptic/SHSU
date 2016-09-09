a614bc900a39ef3c12515077d6ca2bf873c7062a
Cameron <cgree78@gmail.com>
2016-09-09 09:32:33 -0500

    adding a lectures directory for all lecture programs and powerpoints.

---
+++ SHSU_Fall_2016/1437 Programming Fundamentals II/Lectures/Lecture2_code/SubClass2.java
@@ -0,0 +1,13 @@
+public class SubClass2 extends SuperClass2
+{
+   /**
+      Constructor
+   */
+
+   public SubClass2()
+   {
+      super(); 
+      System.out.println("This is the " +
+                 "subclass constructor.");
+   }
+}
