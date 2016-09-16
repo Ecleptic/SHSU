84c5ee3800274e38fcc2594ea497431c2005ea0f
Cameron <cgree78@gmail.com>
2016-09-16 12:44:42 -0500

    fixed my null pointer exception.

--- SHSU_Fall_2016/1437_Programming_Fundamentals_II/Lab_2-SearchString/SearchString.java
+++ SHSU_Fall_2016/1437_Programming_Fundamentals_II/Lab_2-SearchString/SearchString.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 import java.lang.Integer;
+import java.util.concurrent.TimeUnit;
 
 
 
@@ -10,33 +11,60 @@ public static final String TO_STOP = "-1";
 public static final int NOT_FOUND = -1;
 public static final int MAX_SIZE = 1000;
 
-public static String count1;
-public static String count2;
-public static String count3;
+public static int count;
+public static int count1;
+public static int count2;
+public static int count3;
 
+public static String[] wordArray;
+private static String word;
 
-public static void main(String[] args) throws IOException {
-
+public synchronized static void readFile() throws IOException {
         // create textString
         String textString = "";
         // create Scanner stringFile
         Scanner stringFile = new Scanner(new File("sortedStrings.txt"));
-
-
         List<String> temp = new ArrayList<String>();
-
         // while loop
         while (stringFile.hasNext()) {
                 // find next line
-                textString = stringFile.next();
+                textString = stringFile.nextLine();
                 temp.add(textString);
         }
         stringFile.close();
+        wordArray = temp.toArray(new String[temp.size()]);
+}
 
-        String[] wordArray = temp.toArray(new String[0]);
+public static String sequentialNameSearch() throws IOException {
+        System.out.println("We're in sequentialNameSearch");
+        System.out.println(wordArray);  //Seems to return null
+        System.out.println("Word 12: ");
+        System.out.print(wordArray[12]);
+        System.out.println("Word 12234: ");
+        System.out.println(wordArray[12234]);
 
-                System.out.println(wordArray[12]);
-                System.out.println(wordArray[12234]);
+        word = wordArray[count];
+        System.out.println("word: "+ word);
+        return word;
+}
+
+
+public static void main(String[] args) throws IOException, InterruptedException {
+        readFile();
+        if (wordArray==null) {
+                readFile();
+        }
+        sequentialNameSearch();
+
+        // System.out.println("pray for me");
+
+        //do something else
+        // }
+
+
+
+        // sequentialNameSearch(15);
+
 }    // end of main()
 
 }
