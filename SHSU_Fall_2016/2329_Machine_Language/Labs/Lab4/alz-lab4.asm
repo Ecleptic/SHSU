9d0ee225a0e805595c5c98c49252a558f0ae6136
Cameron <cgree78@gmail.com>
2016-10-24 23:25:34 -0500

    edited ALZ's lab is almost finished

--- SHSU_Fall_2016/2329_Machine_Language/Labs/Lab4/alz-lab4.asm
+++ SHSU_Fall_2016/2329_Machine_Language/Labs/Lab4/alz-lab4.asm
@@ -7,199 +7,186 @@
 ; 3.	A procedure RANDOM that receives a number in AX and returns
 ;     the next random number in the AX register.
 
+
 org	  100h
 
 section .data
-msg1:     DB 'Enter a decimal number : $'
-msg2:     DB  0DH,0AH,'The 20 random numbers are :',0DH,0AH,'$'
-again:    DB  0DH,0AH,'Invalid Number. Try Again : $'
-blank:    DB  '    $'
+   prompt_1   db 'Enter a number in base 10 from 1 - 32767: : $'
+   prompt_2   db  0dh,0ah,'The 20 random numbers are :',0dh,0ah,'$'
+   retry      db  0dh,0ah,'Invalid number. Try again : $'
+   blanks     db  '    $'
 
-counter1:  DB  '20 $'
-counter2:  DB  '  $'
+   counter_1  db  20, '$'
+   counter_2  db  '? $'
 
 section .text
-    mov dx, msg1                ; load and display msg1
-    mov ah, 9
-    int 21h
 
-    call read           ; call the procedure read
+     mov dx, prompt_1             ; load and display prompt_1
+     mov ah, 9
+     int 21h
 
-    push ax                      ; push ax onto the stack
+     call read                    ; call the procedure read
 
-    mov dx, msg2                 ; load and display the string msg2
-    mov ah, 9
-    int 21h
+     push ax                      ; push ax onto the stack
 
-    pop ax                       ; pop a value from stack to ax
+     mov dx, prompt_2             ; load and display the string prompt_2
+     mov ah, 9
+     int 21h
 
-    loop_1:                     ; loop label
-      mov cx, 4                  ; set counter2 = 4
-      mov [counter2],cx
+     pop ax                       ; pop a value from stack to ax
 
-      loop_2:                   ; loop label
-        push ax                  ; push ax onto the stack
+     loop_1:                     ; loop label
+       mov byte [counter_2], 1           ; set counter_2=4
 
-        mov dx, blank           ; load and display the string blank
-        mov ah, 9
-        int 21h
+       loop_2:                   ; loop label
+         push ax                  ; push ax onto the stack
 
-        pop ax                   ; pop a value from stack into ax
+         mov dx, blanks           ; load and display the string blanks
+         mov ah, 9
+         int 21h
 
-        call random              ; call the procedure random
-        call write               ; call the procedure write
+         pop ax                   ; pop a value from stack into ax
 
-        dec counter2            ; decrement counter2
-        cmp counter2, 0         ; compare counter2 with 0
-      jne loop_2                ; jump to label loop_2 if counter2!=0
+         call random              ; call the procedure random
+         call write               ; call the procedure write
 
-      dec counter1              ; decrement counter1
-      cmp counter1, 0           ; compare counter1 with 0
-    jne loop_1                  ; jump to label LOOP_1 if COUNTER1!=0
+         dec byte [counter_2]            ; decrement counter_2
+         cmp byte [counter_2], 0         ; compare counter_2 with 0
+       jne loop_2                ; jump to label loop_2 if counter_2!=0
 
-    mov ah, 04CH                  ; DOS function: Exit program
-    mov al, 0                   ; Return exit code value
-    int 21h                     ;Call DOS.  Terminate program
+       dec byte [counter_1]              ; decrement counter_1
+       cmp byte [counter_1], 0           ; compare counter_1 with 0
+     jne loop_1                  ; jump to label loop_1 if counter_1!=0
 
+     mov ah, 4Ch                  ; return control to DOS
+     mov	al,0	                	;exit condition
+     int 21h
 
-
-read:
-   ; this procedure will read a number indecimal form
+ read:
+   ; this procedure will read a number in binary form
    ; input : none
    ; output : store binary number in ax
    ; uses : main
 
-     jmp start                      ; jump to label read
+   jmp start                     ; jump to label start
 
-   error:                        ; jump label
-     mov dx, again                ; load and display the string again
-     mov ah, 9
-     int 21h
+   again:                        ; jump label
 
-   start:                         ; jump label
-     xor bx, bx                     ; clear bx
-     xor cx, cx                     ; clear cx
+   mov dx, retry                  ; load and display the string again
+   mov ah, 9
+   int 21h
 
-     mov ah, 1                      ; set input function
-     int 21h                        ; read a character
+   start:                        ; jump label
 
-     cmp al, "-"                    ; compare al with "-"
-     je minus                      ; jump to label minus if al="-"
+   mov cx, 16                     ; initialize loop counter
+   xor bx, bx                     ; clear bx
+   mov ah, 1                      ; set input function
 
-     cmp al, "+"                    ; compare al with "+"
-     je plus                       ; jump to label plus if al="+"
-     jmp input                     ; jump to label input
-
-   minus:                        ; jump label
-     mov cx, 1                      ; set cx=1
-
-   plus:                         ; jump label
-     int 21h                        ; read a character
-     cmp al, 0Dh                    ; compare AL with CR
-     je end                        ; jump to label end if al=cr
-
-   input:                        ; jump label
-     cmp al, 30h                  ; compare AL with 0
-     jl error                    ; jump to label error if al<0
-
-     cmp al, 39h                  ; compare AL with 9
-     jg error                    ; jump to label error if al>9
-
-     and ax, 000Fh                ; convert ascii to decimal code
-
-     push ax                      ; push ax onto the stack
-
-     mov ax, 10                   ; set ax=10
-     mul bx                       ; set ax=ax*bx
-     mov bx, ax                   ; set bx=ax
-
-     pop ax                       ; pop a value from stack into ax
-
-     add bx, ax                   ; set bx=ax+bx
-
-     mov ah, 1                    ; set input function
+   input:                        ; loop label
      int 21h                      ; read a character
 
-     cmp al, 0Dh                  ; compare AL with CR
-     jne input                   ; jump to label if al!=cr
+     cmp al, 0Dh                  ; compare input and CR
+     je end                      ; jump to label end if input is cr
+
+     cmp al, 0h                  ; compare al with 0
+     jl again                    ; jump to label again if al<0
+
+     and al, 0Fh                  ; convert ascii to decimal code
+     shl bx, 1                    ; shift bx by 1 position towards left
+     or  bl, al                   ; place the input decimal digit in bl
+   loop input                    ; jump to label @input if cx!=0
 
    end:                          ; jump label
 
-     or cx, cx                      ; check cx is 0 or not
-     je exit                       ; jump to label exit if cx=0
-     neg bx                        ; negate bx
+   cmp bx, 0                      ; compare bx with 0
+   jle again                     ; jump tolabel if bx<=0
 
-   exit:                         ; jump label
-     mov ax, bx                    ; set AX=BX
-     ret                            ; return control to the calling procedure
+   mov ax, bx                     ; set ax=bx
 
-write:
-    ; this procedure will display a decimal number
-    ; input : ax
-    ; output : none
-    ; uses : main
+   ret                            ; return control to the calling procedure
 
-      cmp ax, 0                      ; compare ax with 0
-      jge start1                     ; jump to label start if ax>=0
-      mov ah, 2                      ; set output function
-      mov dl, "-"                    ; set dl='-'
-      int 21h                        ; print the character
+ write:
+   ; this procedure will display a number in binary form
+   ; input : ax
+   ; output : display the binary number in ax
+   ; uses : main
 
-      neg ax                         ; take 2's complement of ax
+   push ax                        ; push ax onto the stack
+   push	bx		                    ; save register
+ 	 push	cx		                    ; save loop count
+ 	 push dx		                    ;  save register
 
-    start1:                        ; jump label
+   mov		bx, 10	                 ; div by bx
+ 	 xor		cx, cx	                 ; clear cx for count
 
-      mov bx, ax                     ; set ax=bx
-      xor cx, cx                     ; clear cx
-      mov ax, 10                     ; set ax=10
+   while2:
+   	mov		dx, 0	                   ; setup division - upper half 0
+   	div		bx		                    ;  begin division
 
-    repeat:                       ; loop label
-      xor dx, dx                   ; clear dx
-      div ax                       ; divide ax by ax
-      push dx                      ; push dx onto the stack
-      inc cx                       ; increment cx
-      or bx, bx                    ; take or of bx with bx
-    jne repeat                    ; jump to label repeat if zf=0
+   	push 	dx		                    ; save remainder to stack
 
-    mov ah, 2                      ; set output function
+   	inc		cx	                    	;  increment counter for pop display
+   	cmp 	ax, 0	                   ; check if quotient is 0
+   	jne 	while2                   	;  cont if not 0
+   	mov		ah, 2	                   ; print char fcn
 
-    display:                      ; loop label
-      pop dx                       ; pop a value from stack to dx
-      or dl, 30h                   ; convert decimal to ascii code
-      int 21h                      ; print a character
-      loop display                  ; jump to label display if cx!=0
+   loop1:
+   	pop 	dx		                   ; pop then print
 
-    ret                            ; return control to the calling procedure
+   	or		dl, 30h	                  ; clear top half of dx
+   	int		21h		                   ;  print it
 
+   	loop 	loop1	                   ;  loop times of cx
 
-random:
-; this procedure will generate a random number
-; input : AX
-; output : AX
-; uses : MAIN
+   	pop 	dx		                    ;  restore register
+   	pop		cx	                     	; restore loop count
+   	pop		bx		                    ;  restore register
+   	pop		ax	                     	; restore value
 
-      shl ax, 1                      ; shift ax towards left by 1 position
-      xor dx, dx                     ; clear dx
+   	ret		                       		; return to main prog
 
-      test ax, 8000h                 ; test the 15-bit of ax
-      jne fifteen_bit               ; jump to label fifteen_bit if 15-bit = 1
-      jmp skip_1                    ; jump to label skip_1
+ random:
+   ; this procedure will generate a random number
+   ; input : ax
+   ; output : ax
+   ; uses : main
 
-    fifteen_bit:                  ; jump label
-      mov dh, 1                      ; set dh = 1
+  push	bx		                     ;save bx to stack
 
-    skip_1:                       ; jump label
-      test ax, 4000h                 ; test the 14-bit of ax
-      jne fourteen_bit              ; jump to label fourteen_bit if 14-bit = 1
-      jmp skip_2                    ; jump to label skip_2
+ 	xor		bx, bx	                    ;clear bx for use
+ 	shl		ax, 1	                     ;shift left to set bit 0 later
+ 	rcl		ax, 1	                     ;bit 15 now CF
 
-    fourteen_bit:                 ; jump label
-      mov dl, 1                      ; set DL = 1
+ 	jc		   one		                  ;if CF 1, set bh to 1
+ 	jmp		next                     	;else check bit 14
 
-    skip_2:                       ; jump label
-      xor dh, dl                     ; take xor of dh & dl
-      and ax, 0FFFEH                 ; clear 0-bit of AX
-      or al, dh                      ; set LSB of AL by XOR of DH & DL
-      and ax, 7FFFH                  ; clear 15-bit of AX
+ one:
+   mov	bh, 1                      ;bh holds value of bit15
 
-      ret                            ; return control to the calling procedure
+ next:
+ rcl	ax, 1	                     ;bit 14 now CF
+
+ 	jc		one2                    	;if CF 1, set bl to 1
+ 	jmp 	next2	                   ;otherwise move on
+
+ one2:
+ mov	bl, 1	                     ;bl holds value of bit14
+
+ next2:
+ rcr	ax, 2	                     ;reset to original value
+ 	xor		bh, bl	                 ;perform xor op, then set bit 0 to result
+ 	cmp		bh, 0	                   ;check result of xor
+ 	je		zero	                   ;if 0, clear bit 0
+ 	jmp		not0	                    ;if 1, set bit 0
+
+ zero:
+ 	and		ax, 0FFFEh	            ;clear bit 0
+ 	jmp		bottom		               ;skip set
+
+ not0:
+ or		ax, 00001h	                ;set bit 0
+
+ bottom:
+ 	and		ax, 07FFFh	              ;clear bit 15
+ 	pop		bx			                  ;restore bx
+ 	ret                              ; return control to the calling procedure
