7c24ae1b00835c7d0e5c4e1516122855f8275041
Cameron <cgree78@gmail.com>
2016-11-01 16:35:22 -0500

    machine lab5 finished

--- SHSU_Fall_2016/2329_Machine_Language/Examples/binout.asm
+++ SHSU_Fall_2016/2329_Machine_Language/Examples/binout.asm
@@ -1,18 +1,18 @@
  	org	100h
 
 section .data
-number:  dw	42	
+number:  dw	42
 prompt2: db	"The number  is:    $"
 
-section .text	
-start:  
+section .text
+start:
 
 	mov	bx,[number]	; number to display
-	
+
 out1:	mov	ah,9		; print output label
 	mov	dx,prompt2
-	int 	21h	
-	
+	int 	21h
+
 	mov	cx, 16		; loop counter
 top2:	rol	bx,1		; rotate msb into CF
 	jc	one		; CF = 1?
@@ -22,9 +22,8 @@ one:	mov	dl,'1'		; printing a 1
 print:	mov	ah,2		; print char fcn
 	int	21h		; print it
 	loop	top2		; loop until done
-	
+
 Exit:
 	mov     ah,04Ch           ;DOS function: Exit program
 	mov     al,0              ;Return exit code value
 	int     21h               ;Call DOS.  Terminate program
-
--- SHSU_Fall_2016/2329_Machine_Language/Examples/readstr.asm
+++ SHSU_Fall_2016/2329_Machine_Language/Examples/readstr.asm
@@ -1,17 +1,20 @@
 	org 100h
 section	.bss
-input	resb 	80
+	input	resb 	80
+
+section .dat
+	crlf db 13,10,'$'
 
 section	.text
 	mov	di,input
-	cld                     ;process from left 
+	cld                     ;process from left
 	xor     bx,bx           ;BX holds no. of chars read
 	mov     ah,1            ;input char function
 	int     21h             ;read a char into AL
-WHILE1:	cmp     al,0Dh          ;<CR>? 
+WHILE1:	cmp     al,0Dh          ;<CR>?
 	je      ENDWHLE1	       ;yes, exit
 ;if char is backspace
-	cmp     al,08h          ;is char a backspace? 
+	cmp     al,08h          ;is char a backspace?
 	jne     ELSE1           ;no, store in string
 	dec     di              ;yes, move string ptr back
 	dec     bx              ;decrement char counter
@@ -22,7 +25,7 @@ READ:	int     21h             ;read a char into AL
 	jmp     WHILE1          ;and continue loop
 ENDWHLE1:
 	lea	si,[bx+input-1]	;start of string
-	std                     ;process from left 
+	std                     ;process from left
 	mov     cx,bx       	;cx holds no. of chars
 	jcxz    ENDFOR          ;exit if none
 	mov     ah,2            ;display char function
@@ -32,8 +35,20 @@ TOP:
 	int     21h             ;display character
 	loop    TOP             ;loop until done
 ENDFOR:
-	
+	mov dx, crlf			;carriage return
+	mov ah,9
+	int 21h
+	mov si, input
+	cld
+	mov cx,bx
+	jcxz ENDFOR2
+	mov ah,2
+
+TOP2:
+	lodsb
+	mov dl,al
+	int 21h
+	loop TOP2
+ENDFOR2:
 	mov	ax,4C00h
 	int	21h
-
-	
