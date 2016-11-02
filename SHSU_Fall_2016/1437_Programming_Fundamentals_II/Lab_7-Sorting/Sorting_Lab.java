505394df77df50d41dbd3fe5401d58301ac54983
Cameron <cgree78@gmail.com>
2016-11-02 10:25:41 -0500

    got most of the lab, still need to do merge-sort

--- SHSU_Fall_2016/1437_Programming_Fundamentals_II/Lab_7-Sorting/Sorting_Lab.java
+++ SHSU_Fall_2016/1437_Programming_Fundamentals_II/Lab_7-Sorting/Sorting_Lab.java
@@ -1,60 +1,82 @@
 public class Sorting_Lab {
-    /** Sorting algorithms **/
+/** Sorting algorithms **/
 
-    // Insertion sort.
+// Insertion sort.
 
-    public static void insertionSort(int[] array) {
-        throw new UnsupportedOperationException();
-    }
+public static void insertionSort(int[] arr) {
+        int i, j, newValue;
+        for (i = 1; i < arr.length; i++) {
+                newValue = arr[i];
+                j = i;
+                while (j > 0 && arr[j - 1] > newValue) {
+                        arr[j] = arr[j - 1];
+                        j--;
+                }
+                arr[j] = newValue;
+        }
+}
 
-    // Quicksort.
+// Quicksort.
 
-    public static void quickSort(int[] array) {
+public static void quickSort(int[] array) {
         quickSort(array, 0, array.length - 1);
-    }
+}
 
-    // Quicksort part of an array
-    private static void quickSort(int[] array, int begin, int end) {
-        // Base case.
-        if (begin >= end) return;
+// Quicksort part of an array
+private static void quickSort(int[] array, int begin, int end) {
 
-        // Partition the array.
-        int pivot = partition(array, begin, end);
+        int index = partition(array, begin, end);
+        if (begin < index - 1)
+                quickSort(array, begin, index - 1);
+        if (index < end)
+                quickSort(array, index, end);
+}
 
-        // Now recursively quicksort the two partitions.
-        throw new UnsupportedOperationException();
-    }
+// Partition part of an array, and return the index where the pivot
+// ended up.
+private static int partition(int[] array, int begin, int end) {
 
-    // Partition part of an array, and return the index where the pivot
-    // ended up.
-    private static int partition(int[] array, int begin, int end) {
-        // array[begin] will be the pivot element
-        int low = begin+1;
-        int high = end;
+        int i = begin, j = end;
+        int tmp;
+        int pivot = array[(begin + end) / 2];
 
-        throw new UnsupportedOperationException();
-    }
+        while (i <= j) {
+                while (array[i] < pivot)
+                        i++;
+                while (array[j] > pivot)
+                        j--;
+                if (i <= j) {
+                        tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                        i++;
+                        j--;
+                }
+        };
 
-    // Swap two elements in an array
-    private static void swap(int[] array, int i, int j) {
+        return i;
+}
+
+
+// Swap two elements in an array
+private static void swap(int[] array, int i, int j) {
         int x = array[i];
         array[i] = array[j];
         array[j] = x;
-    }
+}
 
-    // Mergesort.
+// Mergesort.
 
-    public static int[] mergeSort(int[] array) {
+public static int[] mergeSort(int[] array) {
         return mergeSort(array, 0, array.length - 1);
-    }
-
-    // Mergesort part of an array
-    private static int[] mergeSort(int[] array, int begin, int end) {
+}
+// Mergesort part of an array
+private static int[] mergeSort(int[] array, int begin, int end) {
         // Base case: array of length 0 or 1.
         if (begin > end) return new int[0];
         if (begin == end) {
-            int[] result = {array[begin]};
-            return result;
+                int[] result = {array[begin]};
+                return result;
         }
 
         // Midpoint of the array
@@ -63,20 +85,42 @@ public class Sorting_Lab {
         // Recursively sort both halves of the array,
         // then merge the results.
         throw new UnsupportedOperationException();
-    }
+}
 
-    // Merge two sorted arrays into one
-    private static int[] merge(int[] left, int[] right) {
-        // The result array
+// Merge two sorted arrays into one
+private static int[] Merge(int[] left, int[] right)
+{
         int[] result = new int[left.length + right.length];
-        // How far we have got in the result array
-        int nextResult = 0;
-        // How far we have got in the left array
-        int nextLeft = 0;
-        // How far we have got in the right array
-        int nextRight = 0;
 
-        // Idea: repeatedly copy one element from either the left or right array to the result array.
-        throw new UnsupportedOperationException();
-    }
+        int l = 0, r = 0;   // keep track of the current idicies. if we would use a list we would just remove the elements.. but we are using arrays
+        // as long as both arrays have elements, choose the smaller one
+        while (left.length > l && right.length > r)
+        {
+                if (left[l] <= right[r])
+                {
+                        result[l + r] = left[l];
+                        l++;
+                }
+                else
+                {
+                        result[l + r] = right[r];
+                        r++;
+                }
+        }
+
+        // then just add the remaining elements
+        for (int i = l; i < left.length; i++)
+        {
+                result[l + r] = left[i];
+                l++;
+        }
+
+        for (int i = r; i < right.length; i++)
+        {
+                result[l + r] = right[i];
+                r++;
+        }
+
+        return result;
+}
 }
