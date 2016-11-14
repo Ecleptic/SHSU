41585f892a8ddbba16b715c315145bfe10d7639a
Cameron <cgree78@gmail.com>
2016-11-14 09:16:55 -0600

    working on lab6 for machine language. I can return the first string, but that's it.

---
+++ SHSU_Fall_2016/2329_Machine_Language/Labs/Lab6/ckg-lab6.asm
@@ -0,0 +1,49 @@
+org 100h
+section	.bss
+input	resb 	80
+
+section .data
+CRLF: db	0Dh,0Ah
+prompt1: db	"Please enter the first string: $"
+prompt2: db	"Please enter the second string: $"
+prompt3: db	0Dh,0Ah, "The first string is: $"
+prompt4: db	0Dh,0Ah, "The second string is: $"
+
+
+section	.text
+mov	ah,9									; print prompt
+mov	dx,prompt1
+int 	21h
+
+mov	di,input
+cld                     ;process from left
+xor     bx,bx           ;BX holds no. of chars read
+mov     ah,1            ;input char function
+int     21h             ;read a char into AL
+
+WHILE1:
+  cmp     al,0Dh          ;<CR>?
+  je      ENDWHLE1	       ;yes, exit
+;if char is backspace
+  cmp     al,08h          ;is char a backspace?
+  jne     ELSE1           ;no, store in string
+  dec     di              ;yes, move string ptr back
+  dec     bx              ;decrement char counter
+  jmp     READ            ;and go to read another char
+ELSE1:
+  stosb                   ;store char in string
+  inc     bx              ;increment char count
+READ:
+  int     21h             ;read a char into AL
+  jmp     WHILE1          ;and continue loop
+ENDWHLE1:
+mov	ah,9									; print prompt
+mov	dx,prompt3
+int 	21h
+
+  mov byte [di], '$'
+  mov ah,9
+  mov dx, input
+  int 21h
+  mov	ax,4C00h
+  int 21h
