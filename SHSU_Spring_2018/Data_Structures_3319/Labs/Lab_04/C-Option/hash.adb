a4dcf264f53dbfc4be23e75d0b8a7471f4431662
Cameron Green <cgree78@gmail.com>
2018-04-10 21:13:51 -0500

    autocommit

--- SHSU_Spring_2018/Data_Structures_3319/Labs/Lab_04/C-Option/hash.adb
+++ SHSU_Spring_2018/Data_Structures_3319/Labs/Lab_04/C-Option/hash.adb
@@ -26,6 +26,7 @@ procedure hash is
 		end record;
 
 	table: array(0..127) of tableEntry; --because arrays should start at 0 dammit. (but I guess it's okay that strings won't)
+	statsTable: array(0..127) of tableEntry; --because arrays should start at 0 dammit. (but I guess it's okay that strings won't)
 
 	procedure initTable is
 	begin
@@ -36,6 +37,15 @@ procedure hash is
 			table(i).probes := 0;
 		end loop;
 	end initTable;
+	procedure initStatsTable is
+	begin
+		for i in 0..127
+		loop
+			statsTable(i).key := "Undefined_Key   ";
+			statsTable(i).address := 0;
+			statsTable(i).probes := 0;
+		end loop;
+	end initStatsTable;
 
 	function hash(str: string16) return integer is
 		str1: slice := str(3..4);
@@ -76,8 +86,8 @@ procedure hash is
 					knt := 0;
 				elsif timesWrapped > 128
 				then
-					put_line("ERR: Out of space");
-					isPlaced := true;
+					knt := 0;
+					timesWrapped := knt;
 				else
 					knt := knt + 1;
 				end if;
@@ -94,8 +104,8 @@ procedure hash is
 					isPlaced := true;
 				elsif timesWrapped > 128
 				then
-					put_line("ERR: Out of space");
-					isPlaced := true;
+					knt := 0;
+					timesWrapped := knt;
 				else
 					R := R * 5;
 					R := R mod 512;
@@ -111,6 +121,66 @@ procedure hash is
 		end if;
 	end insert;
 
+	procedure insertStatsTable(str: string16; initialProbe: integer; isLinear: boolean) is
+		isPlaced: boolean := false;
+		knt: integer := initialProbe;
+		timesWrapped: integer := 0;
+		wrapKnt: integer := 0;
+		R : integer := 1;
+		numBits: integer := 0;
+	begin
+		if isLinear = true
+		then
+			while isPlaced = false
+			loop
+				if statsTable(knt).key = "Undefined_Key   "
+				then
+					statsTable(knt).key := str;
+					statsTable(knt).address := initialProbe;
+					statsTable(knt).probes := knt + wrapKnt - initialProbe + 1;
+					isPlaced := true;
+				elsif knt = 127
+				then
+					timesWrapped := timesWrapped + 1;
+					wrapKnt := knt + 1;
+					knt := 0;
+				elsif timesWrapped > 128
+				then
+					knt := 0;
+					timesWrapped := knt;
+				else
+					knt := knt + 1;
+				end if;
+			end loop;
+		else
+			wrapKnt := 1;
+			while isPlaced = false
+			loop
+				if statsTable(knt).key = "Undefined_Key   "
+				then
+					statsTable(knt).key := str;
+					statsTable(knt).address := initialProbe;
+					statsTable(knt).probes := wrapKnt;
+					isPlaced := true;
+				elsif timesWrapped > 128
+				then
+					knt := 0;
+					timesWrapped := knt;
+				else
+					R := R * 5;
+					R := R mod 512;
+					numBits := R / 4;
+					knt := initialProbe + numBits;
+					if knt > 127
+					then
+						knt := knt - 127;
+					end if;
+					wrapKnt := wrapKnt + 1;
+				end if;
+			end loop;
+		end if;
+	end insertStatsTable;
+
 	procedure printTable is
 	begin
 		put_line(Character'Val (9) & Character'Val (9) & "Key "  & Character'Val (9) & Character'Val (9) & "Address" & Character'Val (9) & Character'Val (9) & "Probes");
@@ -123,69 +193,113 @@ procedure hash is
 			put_line(Character'Val (9) & Character'Val (9) & "|" & integer'Image(table(i).probes));
 		end loop;
 	end printTable;
-
-	-- TODO: STATS SHOULD COME FROM THE INPUT TIME, NOT PLACE ON TABLE
-	procedure first30Stats is
-	maxProbes: integer := 1; minProbes: integer := 128; avgProbes: float := 0.0; avgKnt: integer := 0;
+	procedure printStatsTable is
 	begin
-		for i in 0..29 loop
-			--put_line(integer'Image(table(i).probes));
-			if table(i).probes > maxProbes
+		put_line(Character'Val (9) & Character'Val (9) & "Key "  & Character'Val (9) & Character'Val (9) & "Address" & Character'Val (9) & Character'Val (9) & "Probes");
+		for i in 0..127 loop
+			put(integer'image(i)); --0,1,2,3
+			put(":" & Character'Val (9) & "|" & Character'Val (9));
+			put(statsTable(i).key);
+			put(" |" & Character'Val (2));
+			put(integer'image(statsTable(i).address));
+			put_line(Character'Val (9) & Character'Val (9) & "|" & integer'Image(statsTable(i).probes));
+		end loop;
+	end printStatsTable;
+
+
+	procedure printStats(isLinear: boolean; givenHash: boolean) is
+		maxProbes: integer := 1; minProbes: integer := 128;
+		avgProbes: float := 0.0; avgKnt: integer := 0;	Line: string(1..17);
+ 		actualLine: string16;
+	begin
+		initStatsTable;
+		Open(Input, In_File, "Words.txt");
+		Set_Line (input, To => 1);
+
+		for i in 0..126
+		loop
+			Line := Get_Line(Input);
+			actualLine := Line(1..16); -- need to remove the newline character
+			if givenHash = true
 			then
-				maxProbes := table(i).probes;
+				-- put_line("not my hash");
+				insertStatsTable(actualLine,hash(actualLine), isLinear);
+			else
+				-- put_line("my hash");
+				insertStatsTable(actualLine,cameronHash(actualLine), isLinear);
 			end if;
-			if  table(i).probes < minProbes and table(i).probes > 0
+			--put_line(integer'Image(statsTable(i)).probes));
+			if i < 30
 			then
-				minProbes := table(i).probes;
-			end if;
-			if table(i).probes > 0
+				for j in 0..127
+				loop
+					if statsTable(j).key = actualLine
+					then
+						if statsTable(j).probes > maxProbes
+						then
+							maxProbes := statsTable(j).probes;
+							-- put_line("Maximum probes for " & statsTable(j).key & "is now" & Integer'Image(statsTable(j).probes));
+						end if;
+						if  statsTable(j).probes < minProbes and statsTable(j).probes > 0
+						then
+							minProbes := statsTable(j).probes;
+							-- put_line("minimum probes for " & statsTable(j).key & "is now" & Integer'Image(statsTable(j).probes));
+						end if;
+						if statsTable(j).probes > 0
+						then
+							avgProbes := (avgProbes) + float(statsTable(j).probes);
+							avgKnt := avgKnt + 1;
+							-- put_line("Avg probes for " & statsTable(j).key & "is now" & Integer'Image(avgKnt));
+						end if;
+					end if;
+				end loop;
+			elsif i = 31
 			then
-				avgProbes := (avgProbes) + float(table(i).probes);
-				avgKnt := avgKnt + 1;
+				avgProbes := avgProbes / float(avgKnt);
+				put_line("Stats for the first 30 entries");
+				put_line("Minimum Probes: " & integer'image(minProbes));
+				put_line("Maximum Probes: " & integer'image(maxProbes));
+				put_line("Average Probes: " & float'image(avgProbes));
+				minProbes := 128;
+				maxProbes := 0;
+				avgProbes := 0.0;
+
+			elsif i > 96 and i < 125
+			then
+				for j in 0..127
+				loop
+					if statsTable(j).key = actualLine
+					then
+						if statsTable(j).probes > maxProbes
+						then
+							maxProbes := statsTable(j).probes;
+							-- put_line("Maximum probes for " & statsTable(j).key & "is now" & Integer'Image(statsTable(j).probes));
+						end if;
+						if  statsTable(j).probes < minProbes and statsTable(j).probes > 0
+						then
+							minProbes := statsTable(j).probes;
+							-- put_line("minimum probes for " & statsTable(j).key & "is now" & Integer'Image(statsTable(j).probes));
+						end if;
+						if statsTable(j).probes > 0
+						then
+							avgProbes := (avgProbes) + float(statsTable(j).probes);
+							avgKnt := avgKnt + 1;
+							-- put_line("avg probes for " & statsTable(j).key & "is now" & Integer'Image(avgKnt));
+						end if;
+				end if;
+				end loop;
+
+			elsif  i = 126
+			then
+				avgProbes := avgProbes / float(avgKnt);
+				put_line("Stats for the last 30 entries");
+				put_line("Minimum Probes: " & integer'image(minProbes));
+				put_line("Maximum Probes: " & integer'image(maxProbes));
+				put_line("Average Probes: " & float'image(avgProbes));
 			end if;
 		end loop;
-		if avgKnt > 0
-		then
-			avgProbes := float(avgProbes) / float(avgKnt);
-		else
-			avgProbes := 0.0;
-		end if;
-		put_line("Stats for the first 30 entries");
-		put_line("Minimum Probes: " & integer'image(minProbes));
-		put_line("Maximum Probes: " & integer'image(maxProbes));
-		put_line("Average Probes: " & float'image(avgProbes));
-	end first30Stats;
-
-	procedure last30Stats is
-	maxProbes: integer := 1; minProbes: integer := 128; avgProbes: float := 0.0; avgKnt: integer := 0;
-	begin
-		for i in 97..127 loop
-			--put_line(integer'Image(table(i).probes));
-			if table(i).probes > maxProbes
-			then
-				maxProbes := table(i).probes;
-			end if;
-			if  table(i).probes < minProbes and table(i).probes > 0
-			then
-				minProbes := table(i).probes;
-			end if;
-			if table(i).probes > 0
-			then
-				avgProbes := (avgProbes) + float(table(i).probes);
-				avgKnt := avgKnt + 1;
-			end if;
-		end loop;
-		if avgKnt > 0
-		then
-			avgProbes := float(avgProbes) / float(avgKnt);
-		else
-			avgProbes := 0.0;
-		end if;
-		put_line("Stats for the last 30 entries");
-		put_line("Minimum Probes: " & integer'image(minProbes));
-		put_line("Maximum Probes: " & integer'image(maxProbes));
-		put_line("Average Probes: " & float'image(avgProbes));
-	end last30Stats;
+		close(input);
+	end printStats;
 
 
 
@@ -194,61 +308,73 @@ procedure hash is
 	actualLine: string16;
 begin
 
-	 initTable;
-	 new_line(3);
+	initTable;
+	new_line(3);
 
 	put_line("Linear Insertion");
-	Open(Input, In_File, "Words.txt");
-	put_line("filling to 50%");
-	for i in 0..63 -- fill to 50%
-	loop
-		Line := Get_Line(Input);
-		actualLine := Line(1..16); -- need to remove the newline character
-		insert(actualLine,hash(actualLine), true);
-	end loop;
-	printTable;
-	first30Stats;
-	last30Stats;
-	new_line(2);
-	put_line("filling to 90%");
-	for i in 64..115 -- fill to 90%
-	loop
-		Line := Get_Line(Input);
-		actualLine := Line(1..16); -- need to remove the new line character
-		insert(actualLine,hash(actualLine), true);
-	end loop;
-	printTable;
-	first30Stats;
-	last30Stats;
-	Close(input);
+		Open(Input, In_File, "Words.txt");
+		Set_Line(input, To => 1);
+		put_line("filling to 50%");
+		for i in 0..63 -- fill to 50%
+		loop
+			Line := Get_Line(Input);
+			actualLine := Line(1..16); -- need to remove the newline character
+			insert(actualLine,hash(actualLine), true);
+		end loop;
+		-- printTable;
+		Close(Input);
+
+	printStats(true,true);
+	initTable;
+
+		Open(Input, In_File, "Words.txt"); 		Set_Line (input, To => 1);
+		new_line(2);
+		put_line("filling to 90%");
+		for i in 0..115 -- fill to 90%
+		loop
+			Line := Get_Line(Input);
+			actualLine := Line(1..16); -- need to remove the new line character
+			insert(actualLine,hash(actualLine), true);
+		end loop;
+		-- printTable;
+		Close(input);
+
+
+	printStats(true, true);
+
 	initTable;
 
 	new_line(3);
 	put_line("Random Insertion");
-	Open(Input, In_File, "Words.txt");
-	put_line("filling to 50%");
-	for i in 0..63 -- fill to 50%
-	loop
-		Line := Get_Line(Input);
-		actualLine := Line(1..16); -- need to remove the new line character
-		insert(actualLine,hash(actualLine), false);
-	end loop;
-	printTable;
-	first30Stats;
-	last30Stats;
-	new_line(2);
-	put_line("filling to 90%");
-	for i in 64..115 -- fill to 90%
-	loop
-		Line := Get_Line(Input);
-		actualLine := Line(1..16); -- need to remove the new line character
-		insert(actualLine,hash(actualLine), true);
-	end loop;
-	printTable;
-	first30Stats;
-	last30Stats;
-	Close(input);
+		Open(Input, In_File, "Words.txt"); 		Set_Line (input, To => 1);
+		put_line("filling to 50%");
+		for i in 0..63 -- fill to 50%
+		loop
+			Line := Get_Line(Input);
+			actualLine := Line(1..16); -- need to remove the new line character
+			insert(actualLine,hash(actualLine), false);
+		end loop;
+		-- printTable;
+		Close(Input);
 
+
+	printStats(false, true);
+	initTable;
+
+		new_line(2);
+		Open(Input, In_File, "Words.txt"); 		Set_Line (input, To => 1);
+		put_line("filling to 90%");
+		for i in 0..115 -- fill to 90%
+		loop
+			Line := Get_Line(Input);
+			actualLine := Line(1..16); -- need to remove the new line character
+			insert(actualLine,hash(actualLine), false);
+		end loop;
+		-- printTable;
+		Close(input);
+
+
+	printStats(false, true);
 
 	initTable;
 	new_line(3);
@@ -256,55 +382,72 @@ begin
 	put_line("WITH MY HASH");
 
 	put_line("Linear Insertion");
-	Open(Input, In_File, "Words.txt");
-	put_line("filling to 50%");
-	for i in 0..63 -- fill to 50%
-	loop
-		Line := Get_Line(Input);
-		actualLine := Line(1..16); -- need to remove the newline character
-		insert(actualLine,cameronHash(actualLine), true);
-	end loop;
-	printTable;
-	first30Stats;
-	last30Stats;
+
+		Open(Input, In_File, "Words.txt"); 		Set_Line (input, To => 1);
+		put_line("filling to 50%");
+		for i in 0..63 -- fill to 50%
+		loop
+			Line := Get_Line(Input);
+			actualLine := Line(1..16); -- need to remove the newline character
+			insert(actualLine,cameronHash(actualLine), true);
+		end loop;
+		-- printTable;
+		Close(Input);
+
+
+	printStats(true, false);
+	initTable;
+
 	new_line(2);
 	put_line("filling to 90%");
-	for i in 64..115 -- fill to 90%
-	loop
-		Line := Get_Line(Input);
-		actualLine := Line(1..16); -- need to remove the new line character
-		insert(actualLine,cameronHash(actualLine), true);
-	end loop;
-	printTable;
-	first30Stats;
-	last30Stats;
-	Close(input);
+
+		Open(Input, In_File, "Words.txt"); 		Set_Line (input, To => 1);
+		for i in 0..115 -- fill to 90%
+		loop
+			Line := Get_Line(Input);
+			actualLine := Line(1..16); -- need to remove the new line character
+			insert(actualLine,hash(actualLine), true);
+		end loop;
+		-- printTable;
+		Close(Input);
+
+
+	printStats(true, false);
+
 	initTable;
 
 	new_line(3);
 	put_line("Random Insertion");
-	Open(Input, In_File, "Words.txt");
-	put_line("filling to 50%");
-	for i in 0..63 -- fill to 50%
-	loop
-		Line := Get_Line(Input);
-		actualLine := Line(1..16); -- need to remove the new line character
-		insert(actualLine,cameronHash(actualLine), false);
-	end loop;
-	printTable;
-	first30Stats;
-	last30Stats;
-	new_line(2);
-	put_line("filling to 90%");
-	for i in 64..115 -- fill to 90%
-	loop
-		Line := Get_Line(Input);
-		actualLine := Line(1..16); -- need to remove the new line character
-		insert(actualLine,cameronHash(actualLine), true);
-	end loop;
-	printTable;
-	first30Stats;
-	last30Stats;
-	Close(input);
+
+		Open(Input, In_File, "Words.txt");
+		Set_Line (input, To => 1);
+		put_line("filling to 50%");
+		for i in 0..63 -- fill to 50%
+		loop
+			Line := Get_Line(Input);
+			actualLine := Line(1..16); -- need to remove the new line character
+			insert(actualLine,cameronHash(actualLine), false);
+		end loop;
+		-- printTable;
+		Close(Input);
+
+
+	printStats(false, false);
+	initTable;
+
+		Open(Input, In_File, "Words.txt"); 		Set_Line (input, To => 1);
+		new_line(2);
+		put_line("filling to 90%");
+		for i in 0..115 -- fill to 90%
+		loop
+			Line := Get_Line(Input);
+			actualLine := Line(1..16); -- need to remove the new line character
+			insert(actualLine,hash(actualLine), false);
+		end loop;
+		-- printTable;
+		Close(input);
+
+
+	printStats(false, false);
 
 end hash;
